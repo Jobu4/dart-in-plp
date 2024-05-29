d9aa68c4c57f46bb65af47693f86fe369f1069d1
Jobu4 <jobmutuku.k@gmail.com>
2024-05-29 18:57:23 +0300

    first Commit

---
+++ functions.dart
@@ -0,0 +1,20 @@
+void main() {
+  double result = myFunction();
+  print(result);
+ 
+  // greet( personToGreet:'job',  greeting:'how do you do');
+}
+
+// String greet({String personToGreet, String greeting}) {
+//   print(" $greeting $personToGreet ");
+// }
+
+double myFunction() {
+  double pi = 3.14159;
+  return pi * 2;
+}
+
+// int getMilk(int money) {
+//   int change = getMilk(money);
+//   return change - 2;
+// }
---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+  String name = "Job";
+  String address = "Kenya";
+  num age = 21;
+  double height = 5.8;
+  bool isMarried = false;
+
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+  var x;
+  var s = x = "boom world";
+  print(x);
+  print(s);
+}
